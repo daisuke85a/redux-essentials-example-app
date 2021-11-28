896e6748721f3a88725230729c4073783d1b05cb
Daisuke Saito <dsaito85a@gmail.com>
2021-11-28 21:13:58 +0900

    [Add] Sorting the Posts List

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
+  const orderdPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map((post) => (
+  const renderedPosts = orderdPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
       <div>
         <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
       </div>
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
